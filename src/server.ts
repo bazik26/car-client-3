c648c9d8e467ab934413f2d730a6be15aab092ea
max <max@kenmoredesign.com>
2025-10-25 12:38:03 +0300

    Fix TypeScript error - convert port to number

--- src/server.ts
+++ src/server.ts
@@ -52,7 +52,7 @@ app.use((req, res, next) => {
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
 if (isMainModule(import.meta.url)) {
-  const port = process.env['PORT'] || 4000;
+  const port = parseInt(process.env['PORT'] || '4000', 10);
   app.listen(port, '0.0.0.0', (error) => {
     if (error) {
       throw error;
