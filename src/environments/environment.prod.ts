06b13124b31d50411e4994300b91601d3b41a389
zigaf <zigaf321@gmail.com>
2025-11-28 22:36:34 +0200

    Fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   API_URL: 'https://car-api-production.up.railway.app',
   SITE_URL: 'https://prime-autos.ru',
-  PROJECT_ID: 'office_1' // Идентификатор офиса/проекта
+  PROJECT_ID: 'office_2' // Идентификатор офиса/проекта
 };
